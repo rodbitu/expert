7830a03c89591beab1d4da2dee073890930daf2b
Rodrigo Bitu <rodrigo.moraes@enacom.com.br>
2023-12-05 22:48:09 -0300

    feat: adjust index.py

--- index.py
+++ index.py
@@ -1,70 +1,81 @@
-class BaseConhecimento:
-    def __init__(self):
-        self.conhecimento = {
-            'não liga': {
-                'mensagem_erro': {
-                    'sim': 'Pesquise o erro específico ou contate o suporte do software.',
-                    'não': 'Tente reiniciar o dispositivo.'
-                },
-                'sem_mensagem_erro': 'Verifique se está corretamente ligado à fonte de energia.'
-            },
-            'problema_software': {
-                'reiniciar': {
-                    'sim': 'Reinstale o software problemático.',
-                    'não': 'Reinicie o dispositivo.'
-                },
-                'sem_reiniciar': 'Verifique atualizações disponíveis para o software.'
-            },
-            'internet_lenta': {
-                'router_reiniciado': {
-                    'sim': 'Verifique se outros dispositivos também estão com internet lenta. Se sim, pode ser um problema com seu provedor de internet.',
-                    'não': 'Tente reiniciar o router.'
-                },
-                'sem_router_reiniciado': 'Verifique se há muitos dispositivos conectados à mesma rede.'
-            },
-            'tela_azul': {
-                'software_recente': {
-                    'sim': 'Desinstale o software recentemente adicionado e veja se o problema persiste.',
-                    'não': 'Tente atualizar seus drivers e o sistema operacional.'
-                },
-                'sem_software_recente': 'Considere realizar uma restauração do sistema para um ponto anterior ao início do problema.'
-            }
-            # Adicione mais cenários conforme necessário
-        }
-
-    def obter_solucao(self, problema, respostas):
-        solucao = self.conhecimento
-        for resposta in respostas:
-            solucao = solucao.get(resposta, {})
-        return solucao if isinstance(solucao,
-                                     str) else "Não consegui diagnosticar o problema. Por favor, contate um técnico especializado."
-
-
 class AssistenteSuporteTecnico:
     def __init__(self):
-        self.base_conhecimento = BaseConhecimento()
-        self.perguntas = {
-            'nível_1': "Qual é o problema do dispositivo? (1: Não liga, 2: Problema de software, 3: Internet lenta, 4: Tela azul)",
-            'não liga': "O dispositivo está ligando? (Sim/Não)",
-            'problema_software': "Você já tentou reiniciar o dispositivo? (Sim/Não)",
-            'internet_lenta': "Você já reiniciou o router? (Sim/Não)",
-            'tela_azul': "Você instalou algum software recentemente? (Sim/Não)",
-            # Adicione mais perguntas conforme necessário
-        }
+        self.categorias_problemas = ["Hardware", "Software", "Rede"]
+        self.perguntas_iniciais = [
+            "Em uma escala de 1 a 5, o quanto você está enfrentando problemas com o hardware?",
+            "Em uma escala de 1 a 5, o quanto você está enfrentando problemas com o software?",
+            "Em uma escala de 1 a 5, o quanto você está enfrentando problemas com a rede?"
+        ]
+        self.perguntas_hardware = [
+            "Em uma escala de 1 a 5, o computador está fazendo barulhos estranhos?",
+            "Em uma escala de 1 a 5, o computador está superaquecendo?",
+            "Em uma escala de 1 a 5, há problemas com as conexões externas?",
+            "Em uma escala de 1 a 5, o computador está reiniciando sozinho?"
+        ]
+        self.perguntas_software = [
+            "Em uma escala de 1 a 5, o software está travando frequentemente?",
+            "Em uma escala de 1 a 5, há problemas com a instalação de programas?",
+            "Em uma escala de 1 a 5, o sistema operacional está atualizado?",
+            "Em uma escala de 1 a 5, você está tendo problemas com vírus ou malware?"
+        ]
+        self.perguntas_rede = [
+            "Em uma escala de 1 a 5, a internet está lenta?",
+            "Em uma escala de 1 a 5, há problemas para conectar à rede Wi-Fi?",
+            "Em uma escala de 1 a 5, a conexão está caindo frequentemente?",
+            "Em uma escala de 1 a 5, há problemas com a configuração de rede?"
+        ]
+
+    def fornecer_solucao(self, categoria):
+        if categoria == "Hardware":
+            return (
+                "Parece que você tem um problema de Hardware. Verifique se todos os cabos estão conectados corretamente, se o computador está limpo e livre de poeira, e se os componentes internos não estão superaquecendo."
+            )
+        elif categoria == "Software":
+            return (
+                "Seu problema parece ser de Software. Certifique-se de que todos os programas estão atualizados, faça uma varredura completa com um antivírus e verifique se não há conflitos de software ou problemas de drivers."
+            )
+        else:
+            return (
+                "Você está enfrentando problemas de Rede. Tente reiniciar o roteador, verifique se a senha da Wi-Fi está correta e confira as configurações de rede no seu dispositivo."
+                )
+
+    def fazer_pergunta(self, pergunta):
+        while True:
+            resposta = input(
+                f"{pergunta} (Digite um número de 1 a 5, onde 1 é pouca severidade e 5 é muita severidade): ")
+            try:
+                resposta = int(resposta)
+                if 1 <= resposta <= 5:
+                    return resposta
+                else:
+                    print("Desculpe, essa não é uma opção válida.")
+            except ValueError:
+                print("Desculpe, não entendi. Por favor, digite um número de 1 a 5.")
 
     def interagir(self):
-        print("Olá! Sou o Assistente de Suporte Técnico. Vou te ajudar a diagnosticar o problema do seu dispositivo.")
-        problemas = {'1': 'não liga', '2': 'problema_software', '3': 'internet_lenta', '4': 'tela_azul'}
-        problema_escolhido = problemas[input(self.perguntas['nível_1'] + " ")]
+        print("Olá! Sou seu assistente de suporte técnico. Vamos diagnosticar o seu problema.")
+        severidades = [0, 0, 0]  # Severidades para hardware, software e rede
+        for i, pergunta in enumerate(self.perguntas_iniciais):
+            resposta = self.fazer_pergunta(pergunta)
+            severidades[i] += resposta
 
-        respostas = []
-        for chave in [problema_escolhido, problema_escolhido]:
-            resposta = input(self.perguntas[chave] + " ").lower()
-            chave_resposta = 'mensagem_erro' if chave == 'não liga' and resposta == 'sim' else resposta
-            respostas.append(chave_resposta)
+        # Determinar a categoria de problema com maior severidade
+        categoria_maior_severidade = self.categorias_problemas[severidades.index(max(severidades))]
+        if categoria_maior_severidade == "Hardware":
+            perguntas = self.perguntas_hardware
+        elif categoria_maior_severidade == "Software":
+            perguntas = self.perguntas_software
+        else:
+            perguntas = self.perguntas_rede
 
-        sugestao = self.base_conhecimento.obter_solucao(problema_escolhido, respostas)
-        print(f"Sugestão: {sugestao}")
+        # Fazer perguntas específicas da categoria
+        for pergunta in perguntas:
+            resposta = self.fazer_pergunta(pergunta)
+            severidades[self.categorias_problemas.index(categoria_maior_severidade)] += resposta
+
+        categoria_problema = self.categorias_problemas[severidades.index(max(severidades))]
+
+        print(self.fornecer_solucao(categoria_problema))
 
 
 assistente = AssistenteSuporteTecnico()
